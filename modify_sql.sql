c032ec3cfe85889b93eb3d502b5eae1cfaa255c4
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2022-06-16 04:58:06 -0500

    update db

--- modify_sql.sql
+++ modify_sql.sql
@@ -1,6 +1,5 @@
 update t_sgcv_opciones SET estado = 0 WHERE id in (2,3);
 update t_sgcv_opciones SET opcion = 'Matriz', url_img = 'cil-apps' WHERE id = 4;
+ALTER TABLE t_sgcv_actividades ADD COLUMN `cumplido` TINYINT(1) DEFAULT 0 AFTER doc_adjunto_id;
 
 -- Everything on top of this on Live Server
-
-ALTER TABLE t_sgcv_actividades ADD COLUMN `cumplido` TINYINT(1) DEFAULT 0 AFTER doc_adjunto_id;
