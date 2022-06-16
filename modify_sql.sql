c18c080f5d67ae882b13ee6186367ec1e9569605
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2022-06-16 00:18:34 -0500

    update on db

--- modify_sql.sql
+++ modify_sql.sql
@@ -1,4 +1,4 @@
--- Everything on top of this on Live Server
-
 update t_sgcv_opciones SET estado = 0 WHERE id in (2,3);
 update t_sgcv_opciones SET opcion = 'Matriz', url_img = 'cil-apps' WHERE id = 4;
+
+-- Everything on top of this on Live Server
