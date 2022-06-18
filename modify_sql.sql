35095b111e04a5663c4b2c6f54c4f865a58b4fb8
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2022-06-18 07:46:54 -0500

    update db sql

--- modify_sql.sql
+++ modify_sql.sql
@@ -2,12 +2,10 @@ update t_sgcv_opciones SET estado = 0 WHERE id in (2,3);
 update t_sgcv_opciones SET opcion = 'Matriz', url_img = 'cil-apps' WHERE id = 4;
 ALTER TABLE t_sgcv_actividades ADD COLUMN `cumplido` TINYINT(1) DEFAULT 0 AFTER doc_adjunto_id;
 
--- Everything on top of this on Live Server
-
 ALTER TABLE t_sgcv_actividades DROP FOREIGN KEY fk_activities_documents1; 
 ALTER TABLE t_sgcv_actividades DROP COLUMN `doc_adjunto_id`;
 
-CREATE TABLE IF NOT EXISTS `mg_db`.`t_sgcv_act_docs` (
+CREATE TABLE `t_sgcv_act_docs` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `actividad_id` BIGINT NOT NULL,
   `documento_id` BIGINT NOT NULL,
@@ -19,7 +17,9 @@ CREATE TABLE IF NOT EXISTS `mg_db`.`t_sgcv_act_docs` (
   INDEX `fk_t_sgcv_act_docs_T_SGCV_Documentos1_idx` (`documento_id` ASC),
   CONSTRAINT `fk_t_sgcv_act_docs_T_SGCV_Actividades1`
     FOREIGN KEY (`actividad_id`)
-    REFERENCES `mg_db`.`T_SGCV_Actividades` (`id`),
+    REFERENCES `t_sgcv_actividades` (`id`),
   CONSTRAINT `fk_t_sgcv_act_docs_T_SGCV_Documentos1`
     FOREIGN KEY (`documento_id`)
-    REFERENCES `mg_db`.`T_SGCV_Documentos` (`id`));
+    REFERENCES `t_sgcv_documentos` (`id`));
+
+-- Everything on top of this on Live Server
