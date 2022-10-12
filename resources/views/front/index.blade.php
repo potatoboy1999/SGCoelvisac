4380f224e6342d455b2d16c34bdbe6362bd24dc9
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2022-10-11 19:49:19 -0500

    fix style and file integrity

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,217 +1,17 @@
 <!DOCTYPE html>
 <!-- saved from url=(0026)https://disac.info/roger/# -->
-<html lang="es" data-darkreader-mode="dynamic" data-darkreader-scheme="dark">
+<html lang="es">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <style class="darkreader darkreader--fallback" media="screen"></style>
-    <style class="darkreader darkreader--text" media="screen"></style>
-    <style class="darkreader darkreader--invert" media="screen">
-        .jfk-bubble.gtx-bubble,
-        .captcheck_answer_label>input+img,
-        span#closed_text>img[src^="https://www.gstatic.com/images/branding/googlelogo"],
-        span[data-href^="https://www.hcaptcha.com/"]>#icon,
-        #bit-notification-bar-iframe,
-        ::-webkit-calendar-picker-indicator {
-            filter: invert(100%) hue-rotate(180deg) contrast(90%) !important;
-        }
-    </style>
-    <style class="darkreader darkreader--inline" media="screen">
-        [data-darkreader-inline-bgcolor] {
-            background-color: var(--darkreader-inline-bgcolor) !important;
-        }
-
-        [data-darkreader-inline-bgimage] {
-            background-image: var(--darkreader-inline-bgimage) !important;
-        }
-
-        [data-darkreader-inline-border] {
-            border-color: var(--darkreader-inline-border) !important;
-        }
-
-        [data-darkreader-inline-border-bottom] {
-            border-bottom-color: var(--darkreader-inline-border-bottom) !important;
-        }
-
-        [data-darkreader-inline-border-left] {
-            border-left-color: var(--darkreader-inline-border-left) !important;
-        }
-
-        [data-darkreader-inline-border-right] {
-            border-right-color: var(--darkreader-inline-border-right) !important;
-        }
-
-        [data-darkreader-inline-border-top] {
-            border-top-color: var(--darkreader-inline-border-top) !important;
-        }
-
-        [data-darkreader-inline-boxshadow] {
-            box-shadow: var(--darkreader-inline-boxshadow) !important;
-        }
-
-        [data-darkreader-inline-color] {
-            color: var(--darkreader-inline-color) !important;
-        }
-
-        [data-darkreader-inline-fill] {
-            fill: var(--darkreader-inline-fill) !important;
-        }
-
-        [data-darkreader-inline-stroke] {
-            stroke: var(--darkreader-inline-stroke) !important;
-        }
-
-        [data-darkreader-inline-outline] {
-            outline-color: var(--darkreader-inline-outline) !important;
-        }
-
-        [data-darkreader-inline-stopcolor] {
-            stop-color: var(--darkreader-inline-stopcolor) !important;
-        }
-    </style>
-    <style class="darkreader darkreader--variables" media="screen">
-        :root {
-            --darkreader-neutral-background: #131516;
-            --darkreader-neutral-text: #d8d4cf;
-            --darkreader-selection-background: #004daa;
-            --darkreader-selection-text: #e8e6e3;
-        }
-    </style>
-    <style class="darkreader darkreader--root-vars" media="screen"></style>
-    <style class="darkreader darkreader--user-agent" media="screen">
-        html {
-            background-color: #181a1b !important;
-        }
-
-        html {
-            color-scheme: dark !important;
-        }
-
-        html, body, input, textarea, select, button {
-            background-color: #181a1b;
-        }
-
-        html, body, input, textarea, select, button {
-            border-color: #736b5e;
-            color: #e8e6e3;
-        }
-
-        a {
-            color: #3391ff;
-        }
-
-        table {
-            border-color: #545b5e;
-        }
-
-        ::placeholder {
-            color: #b2aba1;
-        }
-
-        input:-webkit-autofill,
-        textarea:-webkit-autofill,
-        select:-webkit-autofill {
-            background-color: #404400 !important;
-            color: #e8e6e3 !important;
-        }
-
-        ::-webkit-scrollbar {
-            background-color: #202324;
-            color: #aba499;
-        }
-
-        ::-webkit-scrollbar-thumb {
-            background-color: #454a4d;
-        }
-
-        ::-webkit-scrollbar-thumb:hover {
-            background-color: #575e62;
-        }
-
-        ::-webkit-scrollbar-thumb:active {
-            background-color: #484e51;
-        }
-
-        ::-webkit-scrollbar-corner {
-            background-color: #181a1b;
-        }
-
-        ::selection {
-            background-color: #004daa !important;
-            color: #e8e6e3 !important;
-        }
-
-        ::-moz-selection {
-            background-color: #004daa !important;
-            color: #e8e6e3 !important;
-        }
-    </style>
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Coelvisac</title>
-    <link rel="stylesheet" href="{{asset("css/front/bootstrap.min.css")}}" 
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <style class="darkreader darkreader--sync" media="screen"></style>
+    <link rel="stylesheet" href="{{asset("css/front/bootstrap.min.css")}}" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset("css/front/all.css")}}">
-    <style class="darkreader darkreader--sync" media="screen"></style>
     <link rel="stylesheet" href="{{asset("css/front/owl.carousel.css")}}">
-    <style class="darkreader darkreader--sync" media="screen"></style>
     <link rel="stylesheet" href="{{asset("css/front/owl.theme.default.css")}}">
-    <style class="darkreader darkreader--sync" media="screen"></style>
     <link rel="stylesheet" href="{{asset("css/front/owl.css")}}">
-    <style class="darkreader darkreader--sync" media="screen"></style>
     <link rel="stylesheet" href="{{asset("css/front/estilos.css")}}">
-    <style class="darkreader darkreader--sync" media="screen"></style>
-    <meta name="darkreader" content="47219b4d6f1e43c9a96c43ce307d9014">
-    <style class="darkreader darkreader--override" media="screen">
-        .vimvixen-hint {
-            background-color: #7b5300 !important;
-            border-color: #d8b013 !important;
-            color: #f3e8c8 !important;
-        }
-
-        ::placeholder {
-            opacity: 0.5 !important;
-        }
-
-        #edge-translate-panel-body,
-        .MuiTypography-body1 {
-            color: var(--darkreader-neutral-text) !important;
-        }
-
-        gr-main-header {
-            background-color: #0f3a48 !important;
-        }
-
-        .tou-z65h9k,
-        .tou-mignzq,
-        .tou-1b6i2ox,
-        .tou-lnqlqk {
-            background-color: var(--darkreader-neutral-background) !important;
-        }
-
-        .tou-75mvi {
-            background-color: #032029 !important;
-        }
-
-        .tou-ta9e87,
-        .tou-1w3fhi0,
-        .tou-1b8t2us,
-        .tou-py7lfi,
-        .tou-1lpmd9d,
-        .tou-1frrtv8,
-        .tou-17ezmgn {
-            background-color: #0a0a0a !important;
-        }
-
-        .tou-uknfeu {
-            background-color: #231603 !important;
-        }
-
-        .tou-6i3zyv {
-            background-color: #19576c !important;
-        }
-    </style>
 </head>
 
 <body class="home">
@@ -314,11 +114,9 @@
         </div>
     </div>
 
-    <script src="{{asset("js/front/popper.min.js")}}"
-        integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+    <script src="{{asset("js/front/popper.min.js")}}" crossorigin="anonymous">
     </script>
-    <script src="{{asset("js/front/bootstrap.min.js")}}"
-        integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous">
+    <script src="{{asset("js/front/bootstrap.min.js")}}" crossorigin="anonymous">
     </script>
     <script src="{{asset("js/front/jquery-3.2.1.min.js")}}"></script>
     <script src="{{asset("js/front/owl.carousel.js")}}"></script>
